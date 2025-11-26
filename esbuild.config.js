694c9395a56bd3c1fd970456044ff97f72307e46
Damilare Osibanjo <141376183+Dev-Dami@users.noreply.github.com>
2025-11-26 01:19:24 +0100

    before debug

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,12 +1,25 @@
 const { build } = require("esbuild");
 
+// Build CommonJS version
 build({
   entryPoints: ["src/index.ts"],
   bundle: true,
   platform: "node",
-  target: "node16",
+  target: "node20",
   outfile: "dist/index.js",
   format: "cjs",
+  sourcemap: false,
+  minify: true,
+}).catch(() => process.exit(1));
+
+// Build ES module version
+build({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  platform: "node",
+  target: "node20",
+  outfile: "dist/index.mjs",
+  format: "esm",
   sourcemap: false,
   minify: true,
 }).catch(() => process.exit(1));
